d4fc8776c9d7fcfdc8da33607af9db632f18b835
Ed Sweeney <ed@onextent.com>
2024-06-19 10:46:18 -0700

    models

--- comp.py
+++ comp.py
@@ -6,7 +6,8 @@ def compare_encodings(example_string: str) -> None:
     # print the example string
     print(f'\nExample string: "{example_string}"')
     # for each encoding, print the # of tokens, the token integers, and the token bytes
-    for encoding_name in ["r50k_base", "p50k_base", "cl100k_base"]:
+    for encoding_name in ["r50k_base", "p50k_base", "cl100k_base",
+                          "o200k_base"]:
         encoding = tiktoken.get_encoding(encoding_name)
         token_integers = encoding.encode(example_string)
         num_tokens = len(token_integers)
